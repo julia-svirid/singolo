2512ca8b7eb37b7f6fc7836dbaed537ad4405aa8
julia-svirid <yulia1903@gmail.com>
2020-03-16 12:15:11 +0300

    feat: add black screen

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+/* Навигация */
+
 const MENU = document.getElementById('menu');
 
-MENU.querySelectorAll('a');
 
 
 MENU.addEventListener('click', (event)=>{
     MENU.querySelectorAll('a').forEach(e=>e.classList.remove('active'));
     event.target.classList.add('active');
 });
+
+/* Смена экрана айфона */
+
+const phone = document.getElementById('vertical__iphone');
+phone.addEventListener('click', ()=>{
+    let black = phone.querySelector('img');
+        black.classList.remove('black__vertical');
+        black.classList.add('black__vertical__none');
+
+});
+
+
+const phone__2 = document.getElementById('horisontal__iphone');
+
+phone__2.addEventListener('click', ()=>{
+    let black__2 = phone__2.querySelector('.black__horisontal');
+    black__2.classList.remove('black__horisontal');
+    black__2.classList.add('black__horisontal__none');
+})
+
+
+
+
+/* отправка формы */
+
