e8a1d55f3787e14b76d1a8935810cac118183314
julia-svirid <yulia1903@gmail.com>
2020-03-29 17:17:43 +0300

    feat: add pic mix

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ MENU.addEventListener('click', (event)=>{
 /* Смена экрана айфона */
 
 const phone = document.getElementById('vertical__iphone');
+
 phone.addEventListener('click', ()=>{
     let black = phone.querySelector('img');
+   
         black.classList.remove('black__vertical');
         black.classList.add('black__vertical__none');
 
-});
+}); 
+
+
 
 
 const phone__2 = document.getElementById('horisontal__iphone');
@@ -76,6 +80,7 @@ let pictures = document.getElementById('pictures__id');
 let pic = pictures.querySelectorAll('.pic');
 
 
+
 pictures.addEventListener('click', (event)=>{
     pictures.querySelectorAll('.pic').forEach(e=>e.classList.remove('active'));
     event.target.classList.add('active');
@@ -83,14 +88,49 @@ pictures.addEventListener('click', (event)=>{
 
 
 
+const picSrc = {
+    0: "./assets/singolo2/1.png",
+    1: "./assets/singolo2/2.png",
+    2: "./assets/singolo2/3.png",
+    3: "./assets/singolo2/4.png",
+    4: "./assets/singolo2/5.png",
+    5: "./assets/singolo2/6.png",
+    6: "./assets/singolo2/7.png",
+    7: "./assets/singolo2/8.png",
+    8: "./assets/singolo2/9.png",
+    9: "./assets/singolo2/10.png",
+    10: "./assets/singolo2/11.png",
+    11: "./assets/singolo2/12.png",
+}
+
+
+
+
+
+
+
+
 nav.addEventListener('click', (event)=>{
     nav.querySelectorAll('a').forEach(e=>e.classList.remove('activated'));
     event.target.classList.add('activated');
 
-    let arr = [];
-    arr.push(pic);
-    let array = arr.sort(()=> Math.random()-0.5);
-    console.log(array);
+    pictures.innerHTML = "";
+    let srcArr = [];
+    for(let i in picSrc){
+        srcArr.push(picSrc[i]);
+    };
+   
+    srcArr.sort(() => Math.random() - 0.5);
+
+
+    for (let i = 0; i<12; i++){
+        picSrc[i] = srcArr[i];
+        let picture = document.createElement('img');
+        picture.classList.add = 'pic';
+        picture.src = picSrc[i];
+        pictures.appendChild(picture);
+    }
+   
 
 }); 
 
@@ -98,6 +138,15 @@ nav.addEventListener('click', (event)=>{
 
 
 
+ 
+/*for (let key in picSrc) {
+
+    let picture = document.createElement('img');
+        picture.classList.add = 'pic';
+        picture.src = picSrc[+key+1];
+        pictures.appendChild(picture);
+    }
+*/
 
 
 
